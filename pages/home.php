19aababdc6fc30c206f534c41917bb5d24beddf9
Denox Ramos <kierwinramos28@gmail.com>
2024-11-01 10:24:27 +0800

    Add files via upload

---
+++ pages/home.php
@@ -0,0 +1,47 @@
+
+<!-- pages/home.php -->
+<div class="parallax-section">
+    <div class="parallax-overlay"></div>
+    <div class="parallax-content">
+    </div>
+    <div class="scroll-down">
+        <div style="font-size: 2rem;">↓</div>
+        <div>Scroll Down</div>
+    </div>
+</div>
+<div class="parallax-section" style="background-image: url('css/img/insp.png');">
+    <div class="parallax-overlay"></div>
+    <div class="parallax-content">
+        <h1 style="font-size: 3rem; text-align:center; font-family:dodger; color:white;">Engr. Denox Creations</h1>
+        <p style="font-size: 1.5rem;margin-bottom: 1.5rem; font-family:despair time; color:white; text-align:center">Computer Engineer</p>
+        <p style="font-size: 3rem; color:white;text-align:center; font-family:despair time straight;">Destroy to Create</p>
+    </div>
+</div>
+
+<div class="portfolio-section" style="background-color:#7b3afd; background-image:url('css/img/bgtransp1.png'); width: 100%;
+    height: 80vh;background-size: cover; /* Makes the image auto-fit the container */
+    background-position: center; /* Centers the image within the container */
+    background-repeat: no-repeat; padding-top:2rem;">
+    <div class="container">
+        <div class="section-title" >
+            <h2 style="font-family: dodger; font-size:3rem; color:#e8b207; ">My Work</h2>
+            <p style="font-family:despair time straight;">Here are some of my recent projects</p>
+        </div>
+        <div class="portfolio-grid">
+            <div class="portfolio-item" style="background-color: black;">
+                <img src="css/img/temp.png" alt="Project 1">
+                <h3 style="color:white; text-align:center; font-family:despair time straight;">Temperature Sensor </h3>
+                
+            </div>
+            <div class="portfolio-item"style="background-color: black;">
+                <img src="https://raketcontent.com/2_fec516ddba.png" alt="Project 2">
+                <h3 style="color:white; text-align:center; font-family:despair time straight;">Moisture Sensor</h3>
+                
+            </div>
+            <div class="portfolio-item"style="background-color: black;">
+                <img src="css/img/aeroponics.png" alt="Project 3">
+                <h3 style="color:white; text-align:center; font-family:despair time straight;">Aeroponics Project</h3>
+            </div>
+        </div>
+    </div>
+</div>
